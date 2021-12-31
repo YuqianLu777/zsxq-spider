15199fb083cbd22a9126fe99f08e9e4177500e57
YuqianLu777 <luyuqian.john@gmail.com>
2021-12-31 16:10:05 +0800

    添加数据库init，text类可存可读

---
+++ db_init.py
@@ -0,0 +1,25 @@
+from peewee import *
+from datetime import date
+
+db = SqliteDatabase('zsxq.db')
+
+class Topic(Model):
+    topic_id = CharField(unique=True)
+    create_time = DateTimeField()
+    group_id = CharField()
+    topic_content = TextField()
+
+    class Meta:
+        database = db # This model uses the "zsxq.db" database.
+
+class File(Model):
+    topic_id = CharField(unique=True)
+    file_name = CharField()
+    file_content = TextField()
+
+    class Meta:
+        database = db # this model uses the "people.db" database
+
+db.connect()
+
+db.create_tables([Topic, File])
