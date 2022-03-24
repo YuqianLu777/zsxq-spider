92471846c45a8602e7a457525eec99979ac9a013
YuqianLu777 <luyuqian.john@gmail.com>
2022-03-24 15:54:13 +0800

    可以抓单页数据并存入db，file的内容尚未存入，现在默认为"hello world!"

--- crawler.py
+++ crawler.py
@@ -1,6 +1,4 @@
-#Next Step, 扩展fetch_content方法至多页，参数为browser, group_no, start_time, no_page
-#Step1：翻页并抓取每一页，code完成，没有测试
-#Step2：直接把create_time保存为datetime格式，db中直接存datetime格式（未完成）
+#Next Step, 测试翻页，获取下一页url的位置是否要调整？
 
 from bs4 import BeautifulSoup
 import json
@@ -11,13 +9,16 @@ import time
 from datetime import datetime,timedelta
 from peewee import *
 from db_init import Topic, File
+from db_restart import Topic_test, File_test
 
 global json_buffer
 
 def initiate():
 
     # -*- iniate settings -*-
-#   CMD启动Chrome命令 "c:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=5555 --user-data-dir="C:\selenium\AutomationProfile" 前提是该文件夹已存在 
+#   CMD启动Chrome命令
+#   "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=5555 --disable-popup-blocking --user-data-dir="C:\selenium\AutomationProfile"
+#   前提是该文件夹已存在
     chrome_options = Options()
     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:5555")
     chrome_options.add_argument('--ignore-certificate-errors')
@@ -38,8 +39,7 @@ def initiate():
         signal = input("是否已经扫码登录完毕？ Y/N  ")
         if signal == 'Y' or 'y':
             browser.execute_script("window.open();")
-            handles = browser.window_handles
-            browser.switch_to.window(handles[1])
+            browser.switch_to.window(browser.window_handles[-1])
             browser.get(mypage_url)
             i = 0
             while i < 5:
@@ -48,7 +48,7 @@ def initiate():
 #                    global json_buffer
 #                    json_buffer = html2json(browser.page_source)
                     browser.close()
-                    browser.switch_to.window(handles[0])
+                    browser.switch_to.window(browser.window_handles[-1])
                     print('Selenium启动成功')
                     return browser
                 else:
@@ -94,52 +94,45 @@ def get_download_url(browser, href):
 #        browser.switch_to.window(browser.window_handles[no_handles-1])
         return False
 
-def trans2datetime(input_time:str):
-    date = input_time[0:10]
-    time = input_time[11:23]
-    return(date + ' ' + time)
-
 def fetch_content(browser, group_no, max_page, end_time, start_time):
 #end_time = 抓取的截止时间（包含该时点），start_time = 抓取的开始时间（包含该时点），max_page = 抓取的最大页数
 #browser, group_no, start_time, no_page
 
-'''该方法return一个数组（状态，记录条数，最早时间，最晚时间）
-连接数据库
+    '''该方法return一个数组（状态，记录条数，最早时间，最晚时间）
+    连接数据库
 
-for iterator < max_page:
-    selenium打开新页面
-    browser.get(get_url(end_time))
-    
-    确保正确打开页面
-    if 此时end_time晚于start_time，则断开数据库连接 正常return
-    get页面上的topics数量
-    如果获取到的topics小于20，是否意味着已经到底？？？则获取完页面后断开数据库连接并return
-    获取该页面上最新时间，if该时间早于start_time，遍历找到晚于等于start_time的最早topics，并更新页面上的topics数量
-    
-    循环处理范围内的topics:talk、Q%A、file_url
-    case: talk & Q&A 查重并写入文本数据库
-    case：file_url 查重并写入url数据库
-    
-    更新end_time
-    selenium关闭页面
+    for iterator < max_page:
+        selenium打开新页面
+        browser.get(get_url(end_time))
+        
+        确保正确打开页面
+        if 此时end_time晚于start_time，则断开数据库连接 正常return
+        get页面上的topics数量
+        如果获取到的topics小于20，是否意味着已经到底？？？则获取完页面后断开数据库连接并return
+        获取该页面上最新时间，if该时间早于start_time，遍历找到晚于等于start_time的最早topics，并更新页面上的topics数量
+        
+        循环处理范围内的topics:talk、Q%A、file_url
+        case: talk & Q&A 查重并写入文本数据库
+        case：file_url 查重并写入url数据库
+        
+        更新end_time
+        selenium关闭页面
 
-正常return
-'''
-    
+    正常return
+    '''
 
-    group_url = 'https://api.zsxq.com/v2/groups/' + str(group_no)+'/topics?scope=all&count=20'
+    start_url = 'https://api.zsxq.com/v2/groups/' + str(group_no)+'/topics?scope=all&count=20'
     #db = SqliteDatabase('zsxq.db')
     db = SqliteDatabase('test.db')
     db.connect()
     browser.execute_script("window.open();")
-    handles = browser.window_handles
-    browser.switch_to.window(handles[-1])#[-1]是最新窗口
-    browser.get(group_url)
+    browser.switch_to.window(browser.window_handles[-1])#[-1]是最新窗口
+    browser.get(start_url)
     
     new_talk_list = []
     new_file_list = []
     last_time = datetime.now()
-    for i from range[1:max_page+1]:
+    for ii in range(1,max_page+1):
    
        #获取当前页面json，最多尝试5次，失败则返回异常
         global json_buffer
@@ -161,65 +154,78 @@ for iterator < max_page:
             print(html2json(browser.page_source))
             return False
     
-    #获取一次刷新下的所有内容（file类型的不下载）
-    #f = open("data.txt","w+",encoding="utf-8")
-    #f.write(str(datetime.now())+'\n')
-    for item in topics:
-        content_type = item['type']
-        create_time = trans2datetime(item['create_time'])
-        f.write(create_time+'\n')
-        if content_type == 'talk':
-            text = item['talk']['text']
-            #f.write(text+'\n')
-            new_talk_list.append = ({ 
-                        'topic_id': item['topic_id'],
-                        'create_time': create_time,
-                        'group_id': item['group']['group_id'],
-                        'topic_content': text
-                        })
-            if item['talk'].get('files'):
-                for each_file in item['talk']['files']:
-                    href = 'https://api.zsxq.com/v2/files/' + str(each_file['file_id']) + '/download_url'
-                    download_url = get_download_url(browser, href) 
-                    #f.write(each_file['name']+'\n')
-                    #f.write(download_url+'\n')
-                    new_file_list.append = ({
-                        'topic_id': item['topic_id'],
-                        'file_name': each_file['name'],
-                        'file_content': 'hello world!'
-                        })
-        elif content_type == 'q&a':
-            text = item['question']['text']+'\n'+item['answer']['text']
-            #f.write(text+'\n')
-            new_talk_list.append = ({ 
-                        'topic_id': item['topic_id'],
-                        'create_time': create_time,
-                        'group_id': item['group']['group_id'],
-                        'topic_content': text
-                        })
-        last_time = create_time
-        #f.write('\n')
-    #f.close()
-    #关闭当前tab
-    browser.close()
-    browser.switch_to.window(browser.window_handles[-1])
-    sleep(3)
+        #获取一次刷新下的所有内容（file类型的不下载）
+        #f = open("data.txt","w+",encoding="utf-8")
+        #f.write(str(datetime.now())+'\n')
+        for item in topics:
+            content_type = item['type']
+            create_time = datetime.strptime(item['create_time'], "%Y-%m-%dT%H:%M:%S.%f+0800")
+            print(create_time)
+            #f.write(create_time+'\n')
+            if content_type == 'talk':
+                text = item['talk']['text']
+                #f.write(text+'\n')
+                new_talk_list.append({ 
+                            'topic_id': item['topic_id'],
+                            'create_time': create_time,
+                            'group_id': item['group']['group_id'],
+                            'topic_content': text
+                            })
+                if item['talk'].get('files'):
+                    for each_file in item['talk']['files']:
+                        href = 'https://api.zsxq.com/v2/files/' + str(each_file['file_id']) + '/download_url'
+                        download_url = get_download_url(browser, href) 
+                        #f.write(each_file['name']+'\n')
+                        #f.write(download_url+'\n')
+                        new_file_list.append({
+                            'file_id': each_file['file_id'],
+                            'topic_id': item['topic_id'],
+                            'file_name': each_file['name'],
+                            'file_content': 'hello world!'
+                            })
+            elif content_type == 'q&a':
+                text = item['question']['text']+'\n'+item['answer']['text']
+                #f.write(text+'\n')
+                new_talk_list.append({ 
+                            'topic_id': item['topic_id'],
+                            'create_time': create_time,
+                            'group_id': item['group']['group_id'],
+                            'topic_content': text
+                            })
+            last_time = create_time
+            #f.write('\n')
+        #f.close()
+        #关闭当前tab
+        browser.close()
+        browser.switch_to.window(browser.window_handles[-1])
+        time.sleep(3)
+        
+        #计算新一页的url
+        last_time = last_time + timedelta(microseconds = -1000)
+        next_url = 'https://api.zsxq.com/v2/groups/' + str(group_no)+'/topics?scope=all&count=20&end_time=' + last_time.strftime('%Y-%m-%dT%H%%3A%M%%3A%S.%f')[:-3]+'%2B0800'
+        
+        #打开新tab
+        browser.execute_script("window.open();")
+        browser.switch_to.window(browser.window_handles[-1])
+        browser.get(next_url)
     
-    #计算新一页的end_time
-    last_time = last_time + timedelta(microsecond = -1)
+    #将内容写入数据库
     
-    #打开新tab
-    browser.execute_script("window.open();")
-    handles = browser.window_handles
-    browser.switch_to.window(handles[-1])#[-1]是最新窗口
-    browser.get(group_url)
+    #以下为测试
     
+    Topic_test.truncate_table(restart_identity=True)
+    File_test.truncate_table(restart_identity=True)
+    
+    with db.atomic():
+        Topic_test.insert_many(new_talk_list).execute()
+        File_test.insert_many(new_file_list).execute()
     
     return True
 
 if __name__ == "__main__":
     browser = initiate()
-    fetch_content(browser, 51122528441224, 1)
+    time.sleep(3)
+    fetch_content(browser, 51122528441224, 1, 1, 1)
     time.sleep(999)
     print(link.status_code)
     print(link.content)
